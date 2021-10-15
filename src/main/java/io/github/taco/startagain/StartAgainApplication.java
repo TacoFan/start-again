18b97000bd3a3f04338d446e91533ee6e7c75c8c
fanxx <fanxiaoxia@shulie.io>
2021-10-15 17:55:46 +0800

    update domain

--- src/main/java/io/github/taco/startagain/StartAgainApplication.java
+++ src/main/java/io/github/taco/startagain/StartAgainApplication.java
@@ -1,4 +1,4 @@
-package io.github.startagain;
+package io.github.taco.startagain;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/io/github/taco/startagain/StartAgainApplicationTests.java
+++ src/test/java/io/github/taco/startagain/StartAgainApplicationTests.java
@@ -1,4 +1,4 @@
-package io.github.startagain;
+package io.github.taco.startagain;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
